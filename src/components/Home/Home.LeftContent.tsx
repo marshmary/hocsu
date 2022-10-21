79c3737ccc39c66d5a927112643a85e7ae9560cf
Phúc Ngô <85348631+phucnnh21@users.noreply.github.com>
2022-10-21 20:07:38 +0700

    feat: timeline bar animation on mobile (#10)

--- src/components/Home/Home.LeftContent.tsx
+++ src/components/Home/Home.LeftContent.tsx
@@ -3,7 +3,6 @@ import EventItem from "../EventItem/EventItem";
 
 import "./Home.LeftContent.css";
 import HomeVerticalLine from "./Home.VerticalLine";
-import Timeline from "../Timeline/Timeline";
 
 const data: HistoryEvent[] = [
     {
@@ -12,6 +11,7 @@ const data: HistoryEvent[] = [
         place: "Nga",
         time: "25 tháng 10, 1917",
         content: `Cách mạng Tháng Mười Nga, chính thức được biết đến trong lịch sử Liên Xô với tên gọi Cách mạng xã hội chủ nghĩa tháng Mười vĩ đại và thường được gọi là Cuộc nổi dậy tháng Mười, Cách mạng Bolshevik, hoặc Tháng Mười Đỏ, là một sự kiện lịch sử đánh dấu sự ra đời của nhà nước Nga Xô viết.`,
+        timeline: "2022",
     },
     {
         id: "1",
@@ -24,6 +24,7 @@ const data: HistoryEvent[] = [
          Trước sự chiến đấu dũng mãnh của người Việt, quá nửa quân Nam Hán chết đuối, Hoàng tử Nam Hán là Lưu Hoằng Tháo cũng tử trận. 
          Đây là một trận đánh quan trọng trong lịch sử Việt Nam. 
          Nó đánh dấu cho việc chấm dứt hơn 1000 năm Bắc thuộc của Việt Nam, nối lại quốc thống cho người Việt.`,
+        timeline: "2022",
     },
 
     {
@@ -32,6 +33,7 @@ const data: HistoryEvent[] = [
         place: "VSinger",
         time: "March 22, 2018 - Now",
         content: `彗星の如くあらわれたスターの原石！アイドルVtuberの星街すいせいです！すいちゃん ... 今日もかわいいーーー！`,
+        timeline: "2022",
     },
     {
         id: "4",
@@ -39,6 +41,7 @@ const data: HistoryEvent[] = [
         place: "VSinger",
         time: "March 22, 2018 - Now",
         content: `彗星の如くあらわれたスターの原石！アイドルVtuberの星街すいせいです！すいちゃん ... 今日もかわいいーーー！`,
+        timeline: "2022",
     },
 ];
 
@@ -56,7 +59,6 @@ const LeftContent: FunctionComponent<LeftContentProps> = ({
             className={`${className} px-8 relative lg:px-32 xl:px-40 py-6 lg:py-32 left`}
             style={style}
         >
-            <Timeline />
             <HomeVerticalLine />
             <div className="w-full h-full flex flex-col items-center overflow-y-scroll left-content">
                 {data.map((item) => (
--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,14 +1,44 @@
 import { useState } from "react";
 import TimelineItem from "./Timeline.Item";
 
-const Timeline = () => {
-    const data = ["XVI - XIX", "2016 - 2018", 2019, 2020, 2021, 2022];
+import "./Timeline.style.css";
+
+interface TimelineProps {
+    isTimelineShowing: boolean;
+    timelineRef: React.MutableRefObject<null>;
+}
+
+const Timeline: React.FC<TimelineProps> = ({
+    isTimelineShowing,
+    timelineRef,
+}) => {
+    const data = [
+        "XVI - XIX",
+        "2016 - 2018",
+        2019,
+        2020,
+        2021,
+        2022,
+        2023,
+        "XV - XVI",
+        "2016 - 2019",
+        2024,
+        2025,
+        2026,
+        2027,
+        2030,
+    ];
 
     const [active, setActive] = useState<string | number>(2020);
 
     return (
-        <div className="hidden lg:block h-screen absolute left-0 top-0 pl-4 screen">
-            <ol className="relative border-l border-gray-300 h-full py-10">
+        <div
+            className={`lg:block h-screen fixed top-0 px-5 screen overflow-scroll bg-white z-10 shadow-2xl lg:shadow-none home-timeline ${
+                isTimelineShowing ? "show" : ""
+            }`}
+            ref={timelineRef}
+        >
+            <ol className="relative border-l border-gray-300 py-10">
                 {data.map((item) => (
                     <TimelineItem
                         time={item}

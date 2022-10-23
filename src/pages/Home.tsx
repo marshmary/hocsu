3fdbaf8e3ff52e12dd8ec460f87c45db72e51497
phucnnh21 <phucnnh21@gmail.com>
2022-10-23 20:07:57 +0700

    fix: added Fab in HomePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
+import Fab from "~/components/Fab";
 import HomeLayout from "~/components/Home/Home.Layout";
 import PageLoadAnimation from "~/components/Loading/PageLoadAnimation";
-import { listTimelines } from "~/data/timelines";
 
 const Home = () => {
     return (
         <PageLoadAnimation>
             <HomeLayout />
+            <Fab />
         </PageLoadAnimation>
     );
 };
